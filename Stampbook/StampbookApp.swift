371ae36976cfbf7770410257fa3624228c2f881a
hirooaoy <16730280+hirooaoy@users.noreply.github.com>
2025-10-17 11:27:19 -0700

    Initial Commit

---
+++ Stampbook/StampbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  StampbookApp.swift
+//  Stampbook
+//
+//  Created by Hiroo Aoyama on 10/17/25.
+//
+
+import SwiftUI
+
+@main
+struct StampbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StampbookTests/StampbookTests.swift
@@ -0,0 +1,17 @@
+//
+//  StampbookTests.swift
+//  StampbookTests
+//
+//  Created by Hiroo Aoyama on 10/17/25.
+//
+
+import Testing
+@testable import Stampbook
+
+struct StampbookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
